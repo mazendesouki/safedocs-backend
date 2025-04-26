b3a8c734bc43b78752894a871c6acc338ef0394f
mazendesouki <mazendesouki@gmail.com>
2025-04-26 18:32:24 +0300

    Fix bind address for Render

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => {
   console.log('MongoDB connected');
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`)); 
 })
 .catch(err => console.log(err));
